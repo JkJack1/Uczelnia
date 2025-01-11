43c45f52bb8326b897452cc619997f193093e596
JkJack1 <jk.golaszewski.123@gmail.com>
2025-01-11 01:29:12 +0100

    Zadanie_Spring
    Zadanie spring zgodnie z instrukcją

---
+++ Zadanie_Spring/JanGolaszewski/src/main/java/com/example/JanGolaszewski/JanGolaszewskiApplication.java
@@ -0,0 +1,13 @@
+package com.example.JanGolaszewski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanGolaszewskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanGolaszewskiApplication.class, args);
+	}
+
+}
---
+++ Zadanie_Spring/JanGolaszewski/src/test/java/com/example/JanGolaszewski/JanGolaszewskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JanGolaszewski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanGolaszewskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
